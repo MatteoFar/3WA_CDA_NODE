7b824dca79af028a3e2e8ab45dab23edbe6c5f04
MatteoFar <matteo.farigu@gmail.com>
2023-06-19 16:51:36 +0200

    adding pagination to getallbooks route

--- controllers/getAllBooks.js
+++ controllers/getAllBooks.js
@@ -8,9 +8,41 @@ export default function getAllBooks(req, res) {
     try {
         const booksFile = path.join(__dirname, "..", "data", "books.json")
         const books = JSON.parse(fs.readFileSync(booksFile, "utf-8"))
+
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+
+        if(isNaN(page) && isNaN(limit)) {
+            console.log("test")
+            res.status(200).send(books)
+            return
+        }
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+ 
+        const results = {};
+        if (endIndex < books.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        };
+        }
+ 
+        if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        };
+        }
+ 
+        results.results = books.slice(startIndex, endIndex);
+
+        console.log(results)
     
-        res.status(200).send(books)
+        res.status(200).send(results)
     } catch (error) {
+        console.log(error)
         res.status(500).send("Error on sending books data")
     }
 }
